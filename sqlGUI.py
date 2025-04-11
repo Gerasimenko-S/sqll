5147790c6b42287733094c1a510241543271bed8
Gerasimenko-S <gegerasimenko@gmail.com>
2025-04-11 14:33:23 +0600

    проверка пользователей, главное окно, потом перенести мейн в отдельный файл думать над задачами приложения

--- sqlGUI.py
+++ sqlGUI.py
@@ -1,18 +1,27 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout,
                              QMessageBox, QHBoxLayout)
-user1 = ["adminadmin"]
-user2 = ["prof123"]
-user3 = ["void100"]
+
+# База данных пользователей (логин, пароль)
+user_db = [["admin", "admin"], ["prof", "123"], ["void", "100"]]
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('БД Студенты')
+        self.setGeometry(200, 200, 400, 300)
+
+
+        label = QLabel('Добро пожаловать!', self)
+        label.move(50, 50)
 
 
 class LoginWindow(QWidget):
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Авторизация')
         self.setGeometry(200, 200, 300, 250)
-
         self.init_ui()
 
     def init_ui(self):
@@ -25,10 +34,11 @@ class LoginWindow(QWidget):
         self.edit_password = QLineEdit()
         self.edit_password.setPlaceholderText('Введите ваш пароль')
 
+
         self.button_login = QPushButton('Войти')
         self.button_login.clicked.connect(self.on_login_clicked)
 
-        # Создаем layout и добавляем элементы
+
         layout = QVBoxLayout()
 
         layout.addWidget(self.label_login)
@@ -37,7 +47,7 @@ class LoginWindow(QWidget):
         layout.addWidget(self.label_password)
         layout.addWidget(self.edit_password)
 
-        # Горизонтальный layout для кнопки (чтобы она была по центру)
+
         h_layout = QHBoxLayout()
         h_layout.addStretch()
         h_layout.addWidget(self.button_login)
@@ -48,35 +58,20 @@ class LoginWindow(QWidget):
         self.setLayout(layout)
 
     def on_login_clicked(self):
-        global launch
         login = self.edit_login.text()
         password = self.edit_password.text()
+        auth = False
+        for user in user_db:
+            if user[0] == login and user[1] == password:
+                auth = True
+                break
 
-        if password in user1 and login in user1:
-            usermain = 1
-            launch = 1
-        if (password and login) in user2:
-            usermain = 2
-            launch = 1
-        if (password and login) in user3:
-            usermain = 3
-            launch = 0
-        if launch == 1:
+        if auth:
             self.main_window = MainWindow()
             self.main_window.show()
+            self.close()
         else:
-            QMessageBox.warning(self, 'Ошибка', 'Отклоннено в доступе или такой учетной записи не существует ')
-
-
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('БД Студенты')
-        self.setGeometry(200, 200, 400, 300)
-
-        # Можно добавить любой интерфейс в это окно
-        label = QLabel('Добро пожаловать!', self)
-        label.move(50, 50)
+            QMessageBox.warning(self, 'Ошибка', 'Отклонено в доступе или такой учетной записи не существует')
 
 
 if __name__ == '__main__':
